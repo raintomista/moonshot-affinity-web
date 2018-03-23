41387b6705c913545d153e6de9bddd9e923fce53
Rain Tomista <rftomista@gmail.com>
2018-03-23 20:30:16 +0800

    added dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InvitationLoginComponent } from './invitation-login/invitation-login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CampaignsComponent } from './campaigns/campaigns.component';
 
 const routes: Routes = [
-  { path: '', component: InvitationLoginComponent }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, 
+  { path: 'invitation-login', component: InvitationLoginComponent },
+  { path: 'dashboard', component: DashboardComponent,
+    children: [
+      {path: 'campaigns', component: CampaignsComponent}
+    ]
+  },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { InvitationLoginComponent } from './invitation-login/invitation-login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CampaignsComponent } from './campaigns/campaigns.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InvitationLoginComponent
+    InvitationLoginComponent,
+    DashboardComponent,
+    CampaignsComponent
   ],
   imports: [
     BrowserModule,
