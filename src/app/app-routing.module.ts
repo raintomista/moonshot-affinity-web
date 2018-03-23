119b1e15782b3f3bcf5358a65c2c6919df45ab82
giooooo <gbperalta1@up.edu.ph>
2018-03-23 20:10:55 +0800

    Added invitationlogin component to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InvitationLoginComponent } from './invitation-login/invitation-login.component';
 
 const routes: Routes = [
-
+  { path: '', component: InvitationLoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
 import { AppRoutingModule } from './/app-routing.module';
-=======
 import { InvitationLoginComponent } from './invitation-login/invitation-login.component';
->>>>>>> Stashed changes
+
 
 @NgModule({
   declarations: [
